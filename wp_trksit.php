42edb861387e7f8213b323931de145c1a94cbdc2
arshammm <arsham@Arsham-Mirshahs-MacBook-Pro.local>
2013-10-24 17:09:33 -0400

    Attempting to add auto updater

--- wp_trksit.php
+++ wp_trksit.php
@@ -150,4 +150,39 @@
 		}
     
     }
+    
+    
+/*
+ * UPDATING the plugin automatically
+ */    
+add_action( 'init', 'github_plugin_updater_init' );
+function github_plugin_updater_init() {
+
+	include_once 'updater.php';
+	
+	define( 'WP_GITHUB_FORCE_UPDATE', true );
+
+	// note the use of is_admin() to double check that this is happening in the admin
+	if ( is_admin() ) { 
+	
+		$config = array(
+			'slug' => plugin_basename( __FILE__ ),
+			'proper_folder_name' => 'github-updater',
+			'api_url' => 'https://api.github.com/repos/jkudish/WordPress-GitHub-Plugin-Updater',
+			'raw_url' => 'https://raw.github.com/jkudish/WordPress-GitHub-Plugin-Updater/master',
+			'github_url' => 'https://github.com/jkudish/WordPress-GitHub-Plugin-Updater',
+			'zip_url' => 'https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/archive/master.zip',
+			'sslverify' => true,
+			'requires' => '3.0',
+			'tested' => '3.3',
+			'readme' => 'README.md',
+			'access_token' => '',
+		);
+		
+		new WP_GitHub_Updater( $config );
+	
+	}
+
+}
+    
 ?>
