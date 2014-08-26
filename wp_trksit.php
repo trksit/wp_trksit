bc61c75ed17274de69432af58caa9998b262a7c1
Chris Stefanich <stefanich@gmail.com>
2014-08-25 23:30:45 -0400

    raised curl timeout to 15 seconds

--- wp_trksit.php
+++ wp_trksit.php
@@ -199,4 +199,19 @@ function github_plugin_updater_init() {
     );
 	  new WP_GitHub_Updater($config);
 	}
+ }
+
+//Increase http request timeout
+add_filter('http_request_args', 'bal_http_request_args', 100, 1);
+function bal_http_request_args($r) //called on line 237
+{
+	$r['timeout'] = 15;
+	return $r;
+}
+
+add_action('http_api_curl', 'bal_http_api_curl', 100, 1);
+function bal_http_api_curl($handle) //called on line 1315
+{
+	curl_setopt( $handle, CURLOPT_CONNECTTIMEOUT, 15 );
+	curl_setopt( $handle, CURLOPT_TIMEOUT, 15 );
 }
