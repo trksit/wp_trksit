3d959d1f46b440d060a61ca0200cf76357cf0e19
Chris Stefanich <stefanich@gmail.com>
2015-01-14 19:23:40 -0500

    Error message on fresh install

--- wp_trksit_settings.php
+++ wp_trksit_settings.php
@@ -84,8 +84,11 @@ if($_GET['page'] == 'trksit-settings'){
 <?php
 	if($status = get_transient('trksit_status_messages')){
 		echo '<h3>Recent Status Messages</h3>';
-		foreach(maybe_unserialize($status) as $s){
-			echo '<p>'.$s->status_msg . ' on '. date('M d, Y - g:ia', strtotime($s->date_created)) . '</p>';
+		$stats = maybe_unserialize($status);
+		if(count($stats) > 0){
+			foreach($stats as $s){
+				echo '<p>'.$s->status_msg . ' on '. date('M d, Y - g:ia', strtotime($s->date_created)) . '</p>';
+			}
 		}
 		if(get_transient('trksit_url_status_msg')){
 			echo '<p>'.get_transient('trksit_url_status_msg') . '</p>';
