bf45e694c4db51d8772004b574a37d58ca3e1fc2
Derek Cavaliero <derek@derekcavaliero.com>
2015-03-11 17:29:24 -0400

    Adding chrome extension link in setting page. Styling no-data message in datatable.

--- js/main.js
+++ js/main.js
@@ -142,7 +142,10 @@ jQuery( document ).ready( function( $ ){
 			'order' 	   : [[ 0, 'desc' ]],
 			'aoColumnDefs' : [
 						        { 'bSortable': false, 'aTargets': [ 2, 3, 4, 8 ] }
-						     ]
+						     ],
+			'language'     : {
+							 	'emptyTable': 'You don\'t have any link data yet. <a href="/wp-admin/admin.php?page=trksit-generate">Click here to create your first link!</a>'
+			                 }
 
 		});
 
